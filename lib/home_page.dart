93467b1df0598ee3a3fcc37f1cea72407c5ef1f0
Amit <amit.gupta@fleetcor.com>
2023-08-29 01:17:41 +0530

    Update current location option

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:location/location.dart';
 
-const LatLng currentLocation = LatLng(27.435, 82.183);
+const LatLng myLocation = LatLng(26.435, 82.183);
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -12,30 +13,63 @@ class HomePage extends StatefulWidget {
 
 class _HomePageState extends State<HomePage> {
   late GoogleMapController mapController;
-  Map<String, Marker> _markers = {};
+
+  LocationData? currentLocation;
+
+  void getCurrentLocation() {
+    Location location = Location();
+
+    location.getLocation().then((loc) {
+      print('Current location : ${loc.latitude},${loc..longitude}');
+      currentLocation = loc;
+    });
+
+    location.onLocationChanged.listen((newLoc) {
+      currentLocation = newLoc;
+      setState(() {});
+    });
+  }
+
+  @override
+  void initState() {
+    getCurrentLocation();
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: GoogleMap(
-        initialCameraPosition:
-            const CameraPosition(target: currentLocation, zoom: 14),
-        onMapCreated: (controller) {
-          mapController = controller;
-          addMarker('My Home', currentLocation);
-        },
-        markers: _markers.values.toSet(),
+      appBar: AppBar(
+        title: const Text('Google Map Demo'),
+        centerTitle: true,
       ),
+      body: currentLocation == null
+          ? const Center(
+              child: Text('Loading'),
+            )
+          : GoogleMap(
+              initialCameraPosition: CameraPosition(
+                  target:
+                      // myLocation,
+                      LatLng(currentLocation!.latitude!,
+                          currentLocation!.longitude!),
+                  zoom: 14),
+              onMapCreated: (controller) {
+                mapController = controller;
+              },
+              markers: {
+                Marker(
+                  markerId: const MarkerId('Current locatoin'),
+                  position: LatLng(
+                      currentLocation!.latitude!, currentLocation!.longitude!),
+                ),
+                const Marker(
+                    markerId: MarkerId('My Home'),
+                    position: myLocation,
+                    infoWindow:
+                        InfoWindow(title: 'Home', snippet: 'Amit Home')),
+              },
+            ),
     );
-  }
-
-  addMarker(String id, LatLng location) {
-    var marker = Marker(
-        markerId: MarkerId(id),
-        position: location,
-        infoWindow: const InfoWindow(title: 'Home', snippet: 'Amit Home'));
-
-    _markers[id] = marker;
-    setState(() {});
   }
 }
